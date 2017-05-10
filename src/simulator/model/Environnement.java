a7ff2a60bd7df28a33bcd18f416b32325c4bb8f7
CroustiBapt27 <B.Baptistee@gmail.com>
2017-05-10 17:08:52 +0200

    Ajout class back

--- src/simulator/model/Environnement.java
+++ src/simulator/model/Environnement.java
@@ -2,12 +2,14 @@ package simulator.model;
 
 public class Environnement {
 	private int[][] matrice_env;
+	private int taille_x=10, taille_y=10;
+	private int pourcentage_obstacle=5;
 	
-	public Environnement(int largeur_grille, int hauteur_grille, double pourcentage_obstacle) {
-		matrice_env=new int[largeur_grille][hauteur_grille];	
-		for(int i = 0; i < largeur_grille; i++){					//remplissage de la grille
-            for(int j = 0; j < hauteur_grille; j++){
-            	if(i==0 || i==largeur_grille-1||j==0||j==hauteur_grille-1) {	//mise a 0 des bords
+	public Environnement(int taille_x, int taille_y, double pourcentage_obstacle) {
+		matrice_env=new int[taille_x][taille_y];	
+		for(int i = 0; i < taille_x; i++){					//remplissage de la grille
+            for(int j = 0; j < taille_y; j++){
+            	if(i==0 || i==taille_x-1||j==0||j==taille_y-1) {	//mise a 0 des bords
             		matrice_env[i][j]=0;
             	}
             	else if(Math.random()<pourcentage_obstacle) {		//mise a 2 si obstacle
---
+++ src/simulator/model/Vue_Robot.java
@@ -0,0 +1,11 @@
+package simulator.model;
+
+
+import simulator.model.Environnement;
+import simulator.model.Robot;
+import simulator.model.Orientation;
+
+public class Vue_Robot {
+		
+
+}
