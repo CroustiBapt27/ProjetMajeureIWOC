a5b54d8e0a6eef16fd5981c60ae34e10ab7f28ea
quentin.debout <quentin.debout@tpme29.cpe.fr>
2017-05-10 16:28:41 +0200

    robot sim

---
+++ src/simulator/model/Robot.java
@@ -0,0 +1,16 @@
+package simulator.model;
+
+public class Robot {
+	private int x_robot;
+	private int y_robot;
+	private int envi_robot[][];
+	private char orient_robot;
+	
+	public Robot(int x_robot,int y_robot,char orient_robot){
+		this.x_robot=x_robot;
+		this.y_robot=y_robot;
+		this.orient_robot=orient_robot;
+		
+	}
+
+}
