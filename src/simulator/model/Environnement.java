dfd89abf20a13ae0958bab378e0e9d41d7b9b714
CroustiBapt27 <B.Baptistee@gmail.com>
2017-05-12 16:57:57 +0200

    Bapt - Back - création des tests + modif envi

--- src/simulator/model/Environnement.java
+++ src/simulator/model/Environnement.java
@@ -57,7 +57,7 @@ public class Environnement {
             		matrice_env[i][j]=1;
             	}
             	
-                //System.out.print(matrice_env[i][j] + " ");
+                System.out.print(matrice_env[i][j] + " ");
                 
             }
 		}
@@ -68,6 +68,8 @@ public class Environnement {
             for(int j = 0; j < taille_y; j++){
             	//mise a 0 des bords
             		matrice_env[i][j]=0;
+            		
+            		System.out.print(matrice_env[i][j] + " ");
             }
 		}
 	}
