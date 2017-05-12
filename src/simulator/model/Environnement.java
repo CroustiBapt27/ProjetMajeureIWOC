1f4016cffbd32ced3548fc0fa5a2b62edcfafcad
CroustiBapt27 <B.Baptistee@gmail.com>
2017-05-12 14:09:25 +0200

    Bapt - back end maj environnement, robot et mesure

--- src/simulator/model/Environnement.java
+++ src/simulator/model/Environnement.java
@@ -39,11 +39,11 @@ public class Environnement {
 	public void geneEnvironnement (){
 		for(int i = 0; i < taille_x; i++){					//remplissage de la grille
             for(int j = 0; j < taille_y; j++){
-            	if(i==0 || i==taille_x-1||j==0||j==taille_y-1) {	//mise a 0 des bords
-            		matrice_env[i][j]=0;
-            	}
-            	else if(Math.random()<pourcentage_obstacle) {		//mise a 2 si obstacle
+            	if(i==0 || i==taille_x-1||j==0||j==taille_y-1) {	//mise a 2 des bords (pour affichage)
             		matrice_env[i][j]=2;
+            	}
+            	else if(Math.random()<pourcentage_obstacle) {		//mise a 3 si obstacle
+            		matrice_env[i][j]=3;
             	}
             	else{												//mise a 1 -> vide
             		matrice_env[i][j]=1;
@@ -54,6 +54,16 @@ public class Environnement {
             }
 		}
 	}
+	
+	public void geneEnvironnement_vide(){
+		for(int i = 0; i < taille_x; i++){					//remplissage de la grille
+            for(int j = 0; j < taille_y; j++){
+            	//mise a 0 des bords
+            		matrice_env[i][j]=0;
+            }
+		}
+	}
+	
 	public int getCellule(int x,int y) {
 		return (this.matrice_env[x][y]);
 	}
--- src/simulator/model/Robot.java
+++ src/simulator/model/Robot.java
@@ -3,14 +3,16 @@ package simulator.model;
 public class Robot {
 	private int x_robot;
 	private int y_robot;
-	private Environnement envi_robot;
+	private Environnement envi_robot; //matrice 0 case non vu, 1 case vu, 2 case vu avec obstacle
 	private Orientation orient_robot;
+	private Mesures mesures_robot;
 	
 	public Robot(int x_robot,int y_robot,Environnement envi_robot, Orientation orient_robot){
 		this.x_robot=x_robot;
 		this.y_robot=y_robot;
 		this.envi_robot=envi_robot;
 		this.orient_robot=orient_robot;
+		this.mesures_robot = new Mesures();
 	}
 
 	public int getX_robot() {
@@ -28,24 +30,11 @@ public class Robot {
 	public Orientation getOrient_robot() {
 		return orient_robot;
 	}
-	public void deplacementRobot(Deplacement sens_deplacement) {	//rajouter conditions si robot au bords
-		if(Deplacement.UP_ARROW.equals(sens_deplacement)) {
-			this.y_robot++;
-			this.orient_robot=Orientation.HAUT;
-		}
-		if(Deplacement.DOWN_ARROW.equals(sens_deplacement)) {
-			this.y_robot--;
-			this.orient_robot=Orientation.BAS;
-		}
-		if(Deplacement.RIGHT_ARROW.equals(sens_deplacement)) {
-			this.x_robot++;
-			this.orient_robot=Orientation.DROITE;
-		}
-		if(Deplacement.LEFT_ARROW.equals(sens_deplacement)) {
-			this.x_robot--;
-			this.orient_robot=Orientation.GAUCHE;
-		}
+	
+	public Mesures getMesures_robot(){
+		return this.mesures_robot;
 	}
 	
-
+	//etape 2 dans le controler, regarde si le 
+	
 }
